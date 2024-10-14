d743a13942bd26d4aa682e41e9f0ed6dc91ad0e2
Akhil <akhilpraghav@gmail.com>
2024-10-14 16:25:39 +0530

    searchbar add on navbar right side

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
-
-
+import React from 'react';
+import { IoMdSearch } from "react-icons/io";
 
 const MenuLinks = [
   {
@@ -24,34 +22,40 @@ const MenuLinks = [
     name: "Blogs",
     link: "/#blog"
   },
+];
 
-]
 const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
       <div className='py-4'>
-        <div className='container flex'>
-          <div>
+        <div className='container'>
+          <div className='flex justify-between items-center'>
             <a href="#" className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl py-12'>Eshop</a>
-            <div>
+            <div className='hidden lg:block'>
               <ul className='flex items-center gap-4'>
-                {
-                  MenuLinks.map((data,index)=>(
-                    <li>
-                      <a href={data.link}>{data.name}</a>
-                    </li>
-                  )
-
-                  )
-                }
+                {MenuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a href={data.link} className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'>
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
               </ul>
+            </div>
+
+            {/* Right section with search bar */}
+            <div className='flex items-center gap-4 ml-auto'>
+              {/* search bar section */}
+              <div className='relative group hidden sm:block'>
+                <input type="text" placeholder='Search' className='search-bar' />
+                <IoMdSearch className='text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200' />
+              </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
